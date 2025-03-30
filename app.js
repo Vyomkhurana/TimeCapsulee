fa37d47ad7aa3ff29d5f2fb8e67479d63478e2f0
Vyomkhurana <vyomkhurana846@gmail.com>
2025-03-30 18:20:14 +0530

    Optimized codebase: Removed unused files and directories

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ const mongoose = require('./db'); // MongoDB connection
 const cors = require('cors');
 const capsuleRouter = require('./routes/capsules');
 const { startScheduler } = require('./services/scheduler');
-
-const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
@@ -25,18 +23,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
+// API Routes
 app.use('/users', usersRouter);
 app.use('/api/capsules', capsuleRouter);
 
 // Serve Static HTML Pages
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'Html', 'index.html'));
-});
-
-// Redirect /index to root
-app.get('/index', (req, res) => {
-  res.redirect('/');
 });
 
 app.get('/login', (req, res) => {
@@ -94,9 +87,6 @@ app.get('/terms', (req, res) => {
 app.get('/cookies', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'Html', 'cookies.html'));
 });
-
-// API routes
-app.use('/', indexRouter);
 
 // Handle 404 Errors
 app.use((req, res, next) => {
